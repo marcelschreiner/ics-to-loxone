9373a270b5821d1745029db7ba39a2456f316bdb
Marcel Schreiner <40523792+marcelschreiner@users.noreply.github.com>
2024-02-01 21:14:28 +0100

    Change to ics library that causes fewer issues

--- calendar2lox.py
+++ calendar2lox.py
@@ -1,7 +1,8 @@
-import sys
 import requests
-from icalevents.icalevents import events
-from datetime import datetime, timedelta
+import arrow
+import sys
+from ics import Calendar
+from datetime import timedelta
 
 weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"]
 months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "Augsust", "September", "Oktober", "Novemer",
@@ -10,32 +11,39 @@ months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "Augsust
 
 class CalFetch:
     def __init__(self):
-        self.current_time = None
         self.events_raw = []
         self.events = []
 
     def fetch(self, url_calendar, tag):
-        self.current_time = datetime.now()
-        untagged_events = events(url=url_calendar, end=self.current_time + timedelta(days=365))
-        for event in untagged_events:
-            event.tag = tag
-            self.events_raw.append(event)
+        c = Calendar(requests.get(url_calendar).text)
+        time_now = arrow.now()
+
+        time_zone_shift = arrow.now().utcoffset().total_seconds() / 3600
+
+        for event in c.timeline:
+            # Check if event is between now and one year from now
+            if event.end > time_now and event.begin < (time_now + timedelta(days=365)):
+                print(event.name, event.begin.humanize())
+                event.tag = tag
+                # Correct for timezone
+                if not event.all_day:
+                    event.end = event.end.shift(hours=time_zone_shift)
+                    event.begin = event.begin.shift(hours=time_zone_shift)
+                self.events_raw.append(event)
 
     def parse(self):
-        # Sort list for chronological order
-        self.events_raw.sort(key=lambda x: x.start.isoformat())
-
-        date_today = self.current_time.date()
+        time_now = arrow.now()
+        date_today = time_now.date()
         date_tomorrow = date_today + timedelta(days=1)
         date_7_days = date_today + timedelta(days=7)
 
         self.events = []
         for event in self.events_raw:
-            time_event_start = event.start
+            time_event_start = event.begin
             time_event_end = event.end
 
-            date_event = event.start.date()
-            date_event_end = event.end.date()
+            date_event = time_event_start.date()
+            date_event_end = time_event_end.date()
             date_event_duration = time_event_end - time_event_start
 
             if date_today > date_event:
@@ -64,7 +72,7 @@ class CalFetch:
                 # Check if event is a whole day-event
                 if (date_event_duration == timedelta(days=1)) and (
                         time_event_start.strftime("%m/%d/%Y, %H:%M:%S") == date_event.strftime("%m/%d/%Y, %H:%M:%S")):
-                        str_time = f"Ganzer Tag {description_day}"
+                    str_time = f"Ganzer Tag {description_day}"
                 # Check if event is a multiple whole days-event
                 elif (time_event_start.strftime("%m/%d/%Y, %H:%M:%S") == date_event.strftime(
                         "%m/%d/%Y, %H:%M:%S")) and (
@@ -75,7 +83,7 @@ class CalFetch:
             else:
                 str_time = f'{description_day} {time_event_start.strftime("%H:%M")} - {time_event_end.strftime("%H:%M")}'
 
-            self.events.append({'time': f'{str_time}', 'summary': event.summary, 'tag': event.tag})
+            self.events.append({'time': f'{str_time}', 'summary': event.name, 'tag': event.tag})
 
     def send_to_miniserver(self):
         # Get a list of 5 upcoming events
@@ -88,7 +96,8 @@ class CalFetch:
             else:
                 break
         if 0 < len(self.events):
-            requests.get(f"http://USER:PASSWORD@IPADDRESS/dev/sps/io/Kalender {self.events[0]['tag']}/{text_to_send}")
+            requests.get(
+                f"http://USER:PASSWORT@IPADDRESS/dev/sps/io/Kalender {self.events[0]['tag']}/{text_to_send}")
 
 
 if __name__ == '__main__':
